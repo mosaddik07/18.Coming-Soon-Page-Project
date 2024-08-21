13d0ba98d010875f4b6125b5b557df29385abb0a
Mosaddik Billah <mosaddik.billah08@gmail.com>
2024-08-21 10:46:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date('sep 2 ,2024 00:00:00').getTime();
+var countDownDate = new Date('oct 2 ,2024 00:00:00').getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
